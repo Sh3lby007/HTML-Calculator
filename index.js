03927c5dd4282013fe14c0a8efc19c527c2eebff
Sh3lby007 <zhengshaobin@ymail.com>
2022-09-27 23:25:01 +0800

    Include current and previous operation:     Numbers will appear 2 times when clicked which shows 2 layers.     Remove box id as main calculator display.     Changed the del and AC button accordingly.     Add the function for = button but WIP, still not working yet.

--- index.js
+++ index.js
@@ -2,38 +2,6 @@
 //add event listener for any buttons clicked
 // Whenever user clicks the number, it has to show on the calculator display
 // 
-
-// const numberButtons = document.querySelectorAll("[data-number]")
-// const operationButtons = document.querySelectorAll("[data-action]")
-// let display = document.querySelector(".box")
-
-
-// numberButtons.addEventListener("button", showDisplay)
-
-// function showDisplay() {
-//     display.textContent = numberButtons
-
-// }
-// class calculator {
-//     constructor(display) {
-//         this.clear()
-//     }
-
-// }
-
-// clear() {
-//     this.display = " "
-
-// }
-
-// delete () {
-
-// }
-
-
-
-let display = document.getElementById('calculator_display')
-
 const numButtons = document.querySelectorAll('[data-number]')
 
 const opButtons = document.querySelectorAll('[data-action]')
@@ -44,35 +12,64 @@ const deleteButton = document.querySelector('[data-delete]')
 
 const clearButton = document.querySelector('[data-clear]')
 
+const prev = document.querySelector('[data-previous-operand]')
+
+const current = document.querySelector('[data-current-operand]')
+
 // For every button of the numButtons array, do XXX
 // 
 // Since query selector selects all data-number buttons and returns an array,
 // loop through all of them to attach click event handlers one by one.
 for (const button of numButtons)
     // Event handler to append number value to display box on click
-    button.addEventListener('click', (event) =>
-        // display.textContent += event.target.attributes['data-number'].value
+    button.addEventListener('click', (e) => {
         // https://javascript.plainenglish.io/how-to-get-and-set-the-value-of-a-data-attribute-with-javascript-f0a1bd44063d
-        display.textContent += event.target.dataset.number
-    )
+        prev.textContent += e.target.dataset.number
+        current.textContent += e.target.dataset.number
+    })
+
+
+// For every button of the opButtons array, do XXX
 for (const button of opButtons)
 
-    button.addEventListener('click', (event) =>
-        display.textContent += event.target.attributes['data-action'].value
-    )
+    button.addEventListener('click', (event) => {
+        prev.textContent += event.target.attributes['data-action'].value
+    })
 
 
 clearButton.addEventListener('click', clear)
 
 function clear() {
-    display.textContent = " "
+    prev.textContent = " "
+    current.textContent = " "
 }
 
 deleteButton.addEventListener('click', del)
 
 function del() {
-    display.textContent = display.textContent.slice(0, - 1)
+    prev.textContent = current.textContent.slice(0, -1)
+    current.textContent = current.textContent.slice(0, -1)
 }
 
+equalsButton.addEventListener('click', (event) => {
 
-
+    const prev = parseFloat(this.previousOperandTextElement)
+    const current = parseFloat(this.currentOperandTextElement)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (event.target.textContent) {
+        case '+':
+            display.textContent = prev + current
+            break
+        case '-':
+            display.textContent = prev - current
+            break
+        case '*':
+            display.textContent = prev * current
+            break
+        case '÷':
+            display.textContent = prev / current
+            break
+        default:
+            return
+    }
+})
