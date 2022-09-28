38a03de868c10a40ffd3604e1a74c517a3c65329
Sh3lby007 <zhengshaobin@ymail.com>
2022-09-28 23:35:16 +0800

    Working Calculator:     Include Calculator Class and multiple functions that is easy     to follow and track as the functions in the class runs when the     buttons are clicked.     Once the operation buttons are clicked, the previous number clicked     and the operator will appear on the top of box, awaiting user to     input second number.     Changed the operator symbols in html to standardize with JS symbols.

--- index.js
+++ index.js
@@ -2,74 +2,139 @@
 //add event listener for any buttons clicked
 // Whenever user clicks the number, it has to show on the calculator display
 // 
-const numButtons = document.querySelectorAll('[data-number]')
-
-const opButtons = document.querySelectorAll('[data-action]')
-
-const equalsButton = document.querySelector('[data-equal]')
-
-const deleteButton = document.querySelector('[data-delete]')
-
-const clearButton = document.querySelector('[data-clear]')
-
-const prev = document.querySelector('[data-previous-operand]')
-
-const current = document.querySelector('[data-current-operand]')
-
-// For every button of the numButtons array, do XXX
-// 
-// Since query selector selects all data-number buttons and returns an array,
-// loop through all of them to attach click event handlers one by one.
-for (const button of numButtons)
-    // Event handler to append number value to display box on click
-    button.addEventListener('click', (e) => {
-        // https://javascript.plainenglish.io/how-to-get-and-set-the-value-of-a-data-attribute-with-javascript-f0a1bd44063d
-        prev.textContent += e.target.dataset.number
-        current.textContent += e.target.dataset.number
-    })
-
-
-// For every button of the opButtons array, do XXX
-for (const button of opButtons)
-
-    button.addEventListener('click', (event) => {
-        prev.textContent += event.target.attributes['data-action'].value
-    })
-
-
-clearButton.addEventListener('click', clear)
-
-function clear() {
-    prev.textContent = " "
-    current.textContent = " "
-}
-
-deleteButton.addEventListener('click', del)
-
-function del() {
-    prev.textContent = current.textContent.slice(0, -1)
-    current.textContent = current.textContent.slice(0, -1)
-}
-
-equalsButton.addEventListener('click', (event) => {
-
-    const prev = parseFloat(this.previousOperandTextElement)
-    const current = parseFloat(this.currentOperandTextElement)
-    if (isNaN(prev) || isNaN(current)) return
-    switch (event.target.textContent) {
-        case '+':
-            display.textContent = prev + current
-            break
-        case '-':
-            display.textContent = prev - current
-            break
-        case '*':
-            display.textContent = prev * current
-            break
-        case '÷':
-            display.textContent = prev / current
-            break
-        default:
-            return
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+
+    appendNumber(number) {
+        // We only want a single period to be added, therefore the if statement checks
+        // for whether the number has any period, if it has, it will stop the function
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '×':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText =
+            this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
+}
+
+
+const numButtons = document.querySelectorAll('[data-number]')
+const opButtons = document.querySelectorAll('[data-action]')
+const equalsButton = document.querySelector('[data-equal]')
+const deleteButton = document.querySelector('[data-delete]')
+const clearButton = document.querySelector('[data-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+
+// Since querySelectorAll selects all data-number buttons and returns an array,
+// loop through all of them to attach click event handlers one by one.
+for (const button of numButtons)    //For every button of the numButtons array
+
+    // Event handler to append number value to display box on click
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.textContent)
+        calculator.updateDisplay()
+    })
+
+// Since querySelectorAll selects all data-action buttons and returns an array,
+// loop through all of them to attach click event handlers one by one.
+for (const button of opButtons) // For every button of the opButtons array
+
+    // Event handler to append operator value to display box on click
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.textContent)
+        calculator.updateDisplay()
+    })
+
+
+clearButton.addEventListener('click', () => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
 })
